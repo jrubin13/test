4dc788043ee416d7633393dab36f16f270bdfc56
jrubin13 <jrubin@stuy.edu>
2017-11-20 11:38:15 -0500

    added line

--- index.java
+++ index.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println("Hi");
+	System.out.println( "Hello World!" );
         System.exit( 0 ); //success
     }
 }
